35d75212eb4b97aecf3c5b3ee244d5e788259a46
Prathap <prathap4903@gmail.com>
2024-12-02 23:07:23 +0530

    Update index.tsx

--- client/src/app/index.tsx
+++ client/src/app/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Text, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
+
 export default function Page() {
   return (
     <View className="flex flex-1">
@@ -12,6 +13,7 @@ export default function Page() {
     </View>
   );
 }
+
 
 function Content() {
   return (
