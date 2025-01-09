ee37b0ce5df261331645f65126413cb9a2227437
Prathap <prathap4903@gmail.com>
2025-01-09 23:53:54 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ require('dotenv').config();
 
 const app = express();
 
+app.get('/');
+
 const PORT = 5000 || process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
