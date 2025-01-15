6075fd0f467c7c7b7edc783b663340230c3573ac
Prathap-4903 <prathap4903@gmail.com>
2025-01-15 23:07:52 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,5 +10,5 @@ app.get('/', (req, res) => {
 
 const PORT = 5000 || process.env.PORT;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is Running on PORT : ${PORT}`);
 })
