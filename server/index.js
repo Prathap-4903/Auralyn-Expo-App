24c06ba4c0b1b210b98806cf2f98e5e4d0e022f9
Prathap <prathap4903@gmail.com>
2025-01-16 23:11:09 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+//Backend Configuration
 const app = express();
 
+//Routes
 app.get('/', (req, res) => {
     res.send('Hola From Auralyn Server...');
 });
 
+//Running Server
 const PORT = 5000 || process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server is Running on PORT : ${PORT}`);
