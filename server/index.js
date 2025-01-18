b6de5f01c0348138415ff54e254ca4cccec79605
Prathap <prathap4903@gmail.com>
2025-01-18 23:10:13 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 //MongoDB Configuration
 mongoose.connect("mongodb://localhost:27017")
 .then(() => {
-    
+    console.log("MongoDB Connected - Auralyn")
 })
 .catch((err) => {
     
