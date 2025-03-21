0f93bfe728da29ffdd750441bb98ce160612edb6
Prathap <prathap4903@gmail.com>
2025-03-21 23:22:04 +0530

    Update index.tsx

--- client/src/app/index.tsx
+++ client/src/app/index.tsx
@@ -16,7 +16,7 @@ export default function Page() {
 function Content() {
   return (
     <View className="flex-1 ">
-      <View className="py-12 md:py-24 lg:py-32 xl:py-48">
+      <View className="py-12 md:py-24 lg:py-32 xl:py-48 ">
         <View className="px-4 md:px-6">
           <View className="flex flex-col items-center gap-4 text-center">
             <Text
