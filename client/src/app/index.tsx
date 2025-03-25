7fcb20f124c030591aa4e4732269cc5595e9c7e8
Prathap <prathap4903@gmail.com>
2025-03-25 22:25:07 +0530

    Update index.tsx

--- client/src/app/index.tsx
+++ client/src/app/index.tsx
@@ -18,7 +18,7 @@ function Content() {
     <View className="flex-1 ">
       <View className="py-12 md:py-24 lg:py-32 xl:py-48 ">
         <View className="px-4 md:px-6 ">
-          <View className="flex flex-col items-center gap-4 text-center">
+          <View className="flex flex-col items-center gap-4 text-cente r">
             <Text
               role="heading"
               className="text-3xl text-center native:text-5xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl"
