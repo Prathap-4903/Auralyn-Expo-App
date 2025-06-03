8740fc13b5fd34edcdaac54e4ac63b6635660838
Prathap-4903 <prathap4903@gmail.com>
2025-06-03 22:51:30 +0530

    Updated user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,5 +3,5 @@ const express = require('express');
 const userApi = express.Router();
 
 userApi.get('/:username', (req, res) => {
-    res.send("Working Username API");
+    res.send("Working Username API Route");
 });
